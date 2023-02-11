8a5d9d9acb5986493de9136fc679ffad6cb934ea
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-02-11 13:11:48 +0530

    form data handler

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+  const defaultData = {
+    name: "",
+    email: "",
+    message: "",
+  };
+  const [data, setData] = useState(defaultData);
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  console.log(data);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const { name, email, message } = data;
   return (
     <div className="contact">
       <main>
+        <div className="message"></div>
         <h1>Contact Us</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="name">Name</label>
             <input
               type="text"
               name="name"
+              value={name}
               required
               placeholder="Enter Your Name Here"
+              onChange={(e) => handleChange(e)}
             />
           </div>
 
@@ -21,8 +44,10 @@ const Contact = () => {
             <input
               type="text"
               name="email"
+              value={email}
               required
               placeholder="Enter Your Email Here"
+              onChange={handleChange}
             />
           </div>
 
@@ -31,8 +56,10 @@ const Contact = () => {
             <input
               type="text"
               name="message"
+              value={message}
               required
               placeholder="Tell us about your query........."
+              onChange={handleChange}
             />
           </div>
 
