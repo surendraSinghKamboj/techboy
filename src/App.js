4667bc055ecf444e7d706b68017905fb7f765b1f
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-02-11 13:43:47 +0530

    Services component initialize

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './styles/Header.scss';
 import './styles/Footer.scss';
 import './styles/Home.scss';
 import './styles/Contact.scss';
+import './styles/Services.scss';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home"
 import Footer from './components/Footer';
 import Contact from './components/Contact';
+import Services from './components/Services';
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/services' element={<Services />} />
       </Routes>
       <Footer />
     </Router>
